abb4475fb6fc91b9501bdacdbd603210e2d97094
Antonio Favarin Freire <favarinfreire@hotmail.com>
2022-02-01 12:09:42 -0300

    Feat: Conclusão calculadora IMC

--- src/App.js
+++ src/App.js
@@ -5,12 +5,24 @@ export default function App() {
   
   const [peso, setPeso] = useState('');
   const [altura, setAltura] = useState('');
+  const [mensagem, setMensagem] = useState('');
 
   function calcularIMC (){
     // alert("Altura" + altura); //concatenando print em tela com variavel
     const alt = altura /100;
-    const imc = peso 
-  }
+    const imc = peso / (alt * alt);
+    
+    if(imc < 18.6 ){
+      setMensagem("Você esta abaixo do peso! Seu IMC: " + imc.toFixed(2))
+    }else if(imc >= 18.6 && imc < 24.9 ){
+      setMensagem("Peso ideal! Seu IMC: " + imc.toFixed(2))
+    }else if(imc >= 24.9 && imc < 34.9){
+      setMensagem("Você esta levemente acima do peso! Seu IMC: " + imc.toFixed(2))
+    }else if(imc > 34.9){
+      setMensagem("Cuidado Obesidade! Seu IMC: " + imc.toFixed(2))
+
+    }
+  } 
 
 
   return (
@@ -35,7 +47,7 @@ export default function App() {
           Calcular
         </button>
       </div>
-      <h2>Seu imc foi 25, você está abaixo do peso!  </h2>
+      <h2>{mensagem}</h2>
     </div>
   )
 }
